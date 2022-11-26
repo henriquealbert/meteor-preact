9e7517b53ae0a65dceef8ea669fa2c5d6a4a65bd
hschmaiske <ishenriquealbert@gmail.com>
2022-11-26 14:19:25 -0300

    use esBuild and alias instead of babel

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,19 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    preact({
-      babel: {
-        plugins: [
-          ['@babel/plugin-transform-react-jsx', { pragma: 'h' }],
-          [
-            'babel-plugin-jsx-pragmatic',
-            {
-              module: 'preact',
-              import: 'h',
-              export: 'h',
-            },
-          ],
-        ],
-      },
-    }),
-  ],
+  plugins: [preact({})],
   meteor: {
     clientEntry: 'imports/ui/main.jsx',
+  },
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    jsxInject: `import { h, Fragment } from 'preact'`,
+  },
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+    },
   },
 })
